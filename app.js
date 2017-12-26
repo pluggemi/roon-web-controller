e3b0e724d991fa376cbd47ec5f776544fcb4bbda
pluggemi <otaku@asgard.fu>
2017-12-26 10:09:01 -0500

    created dev branch

--- app.js
+++ app.js
@@ -91,9 +91,9 @@ var RoonApiBrowse    = require("node-roon-api-browse");
 var roon = new RoonApi({
     extension_id:        'com.pluggemi.web.controller',
     display_name:        "Web Controller",
-    display_version:     "1.2.3",
+    display_version:     "dev",
     publisher:           'Mike Plugge',
-//     log_level:           'none',
+    log_level:           'none',
     email:               'masked',
     website:             'https://github.com/pluggemi/roon-web-controller',
 
