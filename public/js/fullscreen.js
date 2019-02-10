3956cf556ab08b404aceae43903913e6901e82b1
otaku <otaku@asgard.fu>
2019-02-10 11:02:56 -0500

    misc format and prettification

--- public/js/fullscreen.js
+++ public/js/fullscreen.js
@@ -2,62 +2,61 @@
 var socket = io();
 
 $(document).ready(function() {
-    $("#buttonMenu").html(getSVG('menu'));
+  $("#buttonMenu").html(getSVG("menu"));
 
-    socket.on("pairStatus", function(payload) {
-        var pairEnabled=payload.pairEnabled;
+  socket.on("pairStatus", function(payload) {
+    var pairEnabled = payload.pairEnabled;
 
-        if (pairEnabled === true ) {
-            showSection('nowPlaying');
-        } else {
-            showSection('pairDisabled');
-        }
-    });
-
+    if (pairEnabled === true) {
+      showSection("nowPlaying");
+    } else {
+      showSection("pairDisabled");
+    }
+  });
 });
 
 function showSection(sectionName) {
-    switch (sectionName) {
-        case "nowPlaying":
-            $("#buttonMenu").show();
-            // Show Now Playing screen
-            $("#nowPlaying").show();
-            // Hide inactive sections
-            $("#pairDisabled").hide();
-            $("#libraryBrowser").hide();
-            $('#overlayMainMenu').hide();
-            break;
-        case "libraryBrowser":
-            $("#buttonMenu").show();
-            // Show libraryBrowser
-            $("#libraryBrowser").show();
-            // Hide inactive sections
-            $("#pairDisabled").hide();
-            $("#nowPlaying").hide();
-            $('#overlayMainMenu').hide();
-            break;
-        case "pairDisabled":
-            // Show pairDisabled section
-            $("#pairDisabled").show();
-            // Hide everthing else
-            $("#buttonMenu").hide();
-            $("#libraryBrowser").hide();
-            $("#nowPlaying").hide();
-            $("#pageLoading").hide();
-            break;
-        default:
-            break;
-        }
-    var t = setTimeout(function (){
-        $("#pageLoading").hide();
-    }, 250);
+  switch (sectionName) {
+    case "nowPlaying":
+      $("#buttonMenu").show();
+      // Show Now Playing screen
+      $("#nowPlaying").show();
+      // Hide inactive sections
+      $("#pairDisabled").hide();
+      $("#libraryBrowser").hide();
+      $("#overlayMainMenu").hide();
+      break;
+    case "libraryBrowser":
+      $("#buttonMenu").show();
+      // Show libraryBrowser
+      $("#libraryBrowser").show();
+      // Hide inactive sections
+      $("#pairDisabled").hide();
+      $("#nowPlaying").hide();
+      $("#overlayMainMenu").hide();
+      break;
+    case "pairDisabled":
+      // Show pairDisabled section
+      $("#pairDisabled").show();
+      // Hide everthing else
+      $("#buttonMenu").hide();
+      $("#libraryBrowser").hide();
+      $("#nowPlaying").hide();
+      $("#pageLoading").hide();
+      break;
+    default:
+      break;
+  }
+  var t = setTimeout(function() {
+    $("#pageLoading").hide();
+  }, 250);
 }
 
 function getSVG(cmd) {
-    switch (cmd) {
-        case "menu":
-            return "<svg viewBox=\"0 0 512 512\"><path d=\"M64 128h384v42.667H64V128m0 106.667h384v42.666H64v-42.666m0 106.666h384V384H64v-42.667z\"/></svg>";
-        default:
-            break;
-    }
+  switch (cmd) {
+    case "menu":
+      return '<svg viewBox="0 0 512 512"><path d="M64 128h384v42.667H64V128m0 106.667h384v42.666H64v-42.666m0 106.666h384V384H64v-42.667z"/></svg>';
+    default:
+      break;
+  }
 }
