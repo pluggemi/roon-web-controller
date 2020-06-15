b3a6b2cc002333bb60415313fe15be53d32e0ebc
pluggemi <pluggemi@users.noreply.github.com>
2020-06-14 22:53:45 -0400

    Updated license file and dates

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@
 The MIT License (MIT)
 =====================
 
-Copyright (c) 2019 Mike Plugge
+Copyright (c) 2020 Mike Plugge
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
